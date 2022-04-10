93884126569c329290a6bf133a47028f18f87f52
jyoung0613 <jwyoung0613@gmail.com>
2022-04-10 10:49:07 -0600

    added additional items in Lib and index to use firebase

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Instagram-clone-freecode
+
 creating a react app Instagram clone website
 Adding a change to make sure everything is working as expected.
---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// here I want to import the seed file
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: "AIzaSyAvje3SNzRSARdpq1v-UMFGOaVMb8cqdMg",
+  authDomain: "instagram-9df27.firebaseapp.com",
+  projectId: "instagram-9df27",
+  storageBucket: "instagram-9df27.appspot.com",
+  messagingSenderId: "414159505979",
+  appId: "1:414159505979:web:c05af9946e187846ee6a1e"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// here is where I want to call the seed file (only ONCE!)
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
