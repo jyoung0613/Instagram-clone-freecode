daf82c228f4753b65937e8bca0fd34f29491f121
jyoung0613 <jwyoung0613@gmail.com>
2022-05-01 11:22:14 -0600

    added tailwind css to project

--- src/index.js
+++ src/index.js
@@ -12,19 +12,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// client side rendered app: react (cra)
-  // -> database which is Firebase
-  // -> react-loading-skeleton
-  // tailwind
-
-// folder structure
-  // src
-    // -> components, 
-    // -> constants, 
-    // -> context, 
-    // -> helpers,
-    // -> hooks, 
-    // -> pages,
-    // -> lib (firebase is going to live in here),
-    // -> services (firebase functions in here)
-    // -> styles (tailwind's folder (app/tailwind))
